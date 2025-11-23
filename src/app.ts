54a520c392f014f0520842b9b82314461da922e0
wilder ub30 <urbina153@gmail.com>
2025-11-23 17:03:05 -0500

    Configuracion base de datos

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { envsAdapter } from "./config/envs.adapter";
+import { MongodbInit } from "./database/mongo.initi";
 import { AppRoutes } from "./presentation/routes";
 import { Server } from "./presentation/server";
 
@@ -8,8 +10,13 @@ import { Server } from "./presentation/server";
 
 async function main() {
 
+    await MongodbInit.connect({
+        mongoUrl: envsAdapter.MONGO_URL,
+        dbName: envsAdapter.MONGO_NAME,
+    });
+
     const server = new Server({
-        port: 3000,
+        port: envsAdapter.PORT,
         routes: AppRoutes.routes,
     });
 
---
+++ src/config/envs.adapter.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { get } from 'env-var';
+
+
+
+export const envsAdapter = {
+    PORT: get('PORT').required().asPortNumber(),
+    FRONTED_URL: get('FRONTED_URL').required().asString(),
+    MONGO_PASSWORD: get('MONGO_PASSWORD').required().asString(),
+    MONGO_NAME: get('MONGO_NAME').required().asString(),
+    MONGO_URL: get('MONGO_URL').required().asString(),
+}
